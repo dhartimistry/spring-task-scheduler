540cc4951fb1aeb222143115432d80e82989e26f
Dharti Mistry <70102044+dhartimistry@users.noreply.github.com>
2025-03-18 15:32:01 -0400

    Initial commit

---
+++ src/main/java/com/example/spring_task_scheduler/SpringTaskSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_task_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaskSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTaskSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_task_scheduler/SpringTaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_task_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
